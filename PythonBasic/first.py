e1ec19ff29a45288eb849d9114b5d6160f178f19
afsoar <afsoar@outlook.com>
2015-07-21 14:13:16 +0800

    basic practice in PythonBasic

---
+++ PythonBasic/first.py
@@ -0,0 +1,4 @@
+#coding=utf-8
+#!/usr/bin/python
+print "Hello World!";
+print "你好，世界！";
---
+++ PythonBasic/list.py
@@ -0,0 +1,7 @@
+#coding = utf-8
+#!/usr/bin/python
+
+list = ['abcd',123,"haha"]
+
+print list
+print list[2]
